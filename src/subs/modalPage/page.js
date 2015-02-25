b7d55b8487bf54352ac985cb42a3fa030da9ab24
chen8114@sina.com <chen8114@sina.com>
2015-02-25 15:51:23 +0800

    update page.js callback func

--- src/subs/modalPage/page.js
+++ src/subs/modalPage/page.js
@@ -93,7 +93,9 @@ define( "Page" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , functio
 			} ,
 			getPageModal 		: function( func ){
 				this._pageConfig.eventList.push( function(){
-					if( $.isFunction( func ) && this._pageConfig.belongPageList._pageListConfig.callBack.call( this ) !== false ){ func(); }
+					if( $.isFunction( func ) && 
+						( $.isFunction( this.__pageConfig.belongPageList._pageConfig.callBack ) ? this._pageConfig.belongPageList._pageListConfig.callBack.call( this ) !== false : true )
+					){ func(); }
 				} );
 				if ( this._pageConfig.ready == "ready" ) {
 					this.handlePageEventList();
