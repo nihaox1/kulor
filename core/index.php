70c2a8494ff406b0a79acd2698a1be85653ed2e4
chen8114@sina.com <chen8114@sina.com>
2015-03-30 20:18:35 +0800

    update package.js

--- core/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$rtn 	= json_encode( array( "name"=>( isset( $_POST[ "name" ] ) ? $_POST[ "name" ] : "xxxxxx" ) ) );
-if ( isset( $_GET[ "callback" ] ) ) {
-	$rtn 	= $_GET[ "callback" ] . "(" . $rtn . ")";
-}
-printf( $rtn );
-?>
